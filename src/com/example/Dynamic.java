56bc58e10151bb80e571e4a1db93b38319dda4b3
Junseok <wnstjr2gkgk@gmail.com>
2020-08-04 23:08:53 +0900

    Initial commit for HW

---
+++ src/com/example/Dynamic.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Dynamic {
+	String hobby;
+
+	public String getHobby() {
+		return hobby;
+	}
+
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+	
+}
